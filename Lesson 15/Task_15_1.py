119e1156382e8e0a2d5f20a25ecd09518b6150c1
Dezict <fcupmsvciyvpa@gmail.com>
2025-08-10 18:22:33 +0300

    Для удобства добавил задачи

--- Lesson 15/Task_15_1.py
+++ Lesson 15/Task_15_1.py
@@ -1,4 +1,21 @@
+#  Задание №1
+
+# Есть родительский класс:
+
  
+
+# class Transport:
+
+#    def __init__(self, name, max_speed, mileage):
+
+# self.name = name
+# self.max_speed = max_speed
+# self.mileage = mileage
+ 
+# Создайте объект Autobus, который унаследует все переменные и методы родительского класса Transport и выведете его.
+# Ожидаемый результат вывода:
+
+# Название автомобиля: Renaul Logan Скорость: 180 Пробег: 12
 
 class Transport(object):
 
